074abd7c30cbc43cc6b13d938a344cea079b725b
Al <alvitomuh@gmail.com>
2025-04-04 00:56:09 +0700

    ubah tampilan dashboard customer

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-[rgba(156,160,171,0.60)] w-60 p-3 rounded shadow flex flex-col items-center justify-center">
+<div class="md:w-48 flex flex-col ">
     <div class="w-full aspect-square">
         <img src="{{ $image }}" class="w-full h-full object-cover rounded" alt="{{ $title }}">
     </div>
-    <p class="text-lg text-center font-bold mt-2">{{ $title }}</p>
+    <p class="text-xl text-center font-bold mt-2">{{ $title }}</p>
 </div>
