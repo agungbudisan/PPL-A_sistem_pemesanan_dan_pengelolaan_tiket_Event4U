0bbb5516f91f67aa0453c2a9ae41945c4c3b9d76
Al <alvitomuh@gmail.com>
2025-04-03 21:39:35 +0700

    memisah code navbar dan footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,31 +9,7 @@
 <body class="bg-gray-100 text-gray-900" style="background-image: url('/images/main_bg.png'); background-repeat: repeat;">
 
     <!-- Navbar -->
-<nav class="bg-[#7B0015] text-white py-6">
-    <div class="container mx-auto flex justify-between items-center px-6">
-        <h1 class="text-[#F7ECAC] text-2xl font-bold">Event 4 U</h1>
-        <ul class="flex space-x-6">
-            <!-- Link yang harus berwarna #F7ECAC -->
-            <li><a href="{{ url('/') }}" class="text-[#F7ECAC] hover:text-white">Home</a></li>
-            <li><a href="#" class="text-[#F7ECAC] hover:text-white">Category</a></li>
-            <li><a href="#" class="text-[#F7ECAC] hover:text-white">Contact</a></li>
-
-            @auth
-                <li><a href="{{ route('dashboard') }}" class="hover:text-gray-300">Dashboard</a></li>
-                <li>
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit" class="hover:text-gray-300">Logout</button>
-                    </form>
-                </li>
-            @else
-                <!-- Login & Register tetap putih -->
-                <li><a href="{{ route('login') }}" class="text-white hover:text-gray-300">Login</a></li>
-                <li><a href="{{ route('register') }}" class="text-white hover:text-gray-300">Register</a></li>
-            @endauth
-        </ul>
-    </div>
-</nav>
+    @include('components.navbar')
 
     <!-- Main Content -->
     <main class="container mx-auto mt-8 px-6 bg-repeat">
@@ -176,11 +152,7 @@
     </main>
 
     <!-- Footer -->
-    <footer class="bg-[#7B0015] text-white py-6 mt-6">
-        <div class="text-center">
-            <p class="text-ls">2025 PT Tiket.in Indonesia. Created by Kelompok 4.</p>
-        </div>
-    </footer>
+    @include('components.footer')
 
 </body>
 </html>
